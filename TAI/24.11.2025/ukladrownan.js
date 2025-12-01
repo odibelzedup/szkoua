6675c22755c297493857bffd4bbd9f5ee3ec0be0
Gaben <odibelzedup@gmail.com>
2025-12-01 15:55:27 +0100

    Add files via upload

--- TAI/24.11.2025/ukladrownan.js
+++ TAI/24.11.2025/ukladrownan.js
@@ -1,9 +1,9 @@
-var a1=prompt("Podaj a1:");
-var a2=prompt("Podaj a2:");
-var b1=prompt("Podaj b1:");
-var b2=prompt("Podaj b2:");
-var c1=prompt("Podaj c1:");
-var c2=prompt("Podaj c2:");
+var a1 = parseFloat(prompt("Podaj a1:"));
+var a2 = parseFloat(prompt("Podaj a2:"));
+var b1 = parseFloat(prompt("Podaj b1:"));
+var b2 = parseFloat(prompt("Podaj b2:"));
+var c1 = parseFloat(prompt("Podaj c1:"));
+var c2 = parseFloat(prompt("Podaj c2:"));
 if ((isNaN(a1)) || (isNaN(a2)) || (isNaN(b1)) || (isNaN(b2)) || (isNaN(c1)) || (isNaN(c2))) {
     document.write("<font color='red' size='5'>Jedna lub więcej z podanych wartosci nie jest liczba!</font>");
 }
@@ -12,22 +12,17 @@ else {
     var Wx = c1 * b2 - c2 * b1;
     var Wy = a1 * c2 - a2 * c1;
 
-    if (W != 0) {
-    var Wx / W;
-    var Wy / W;
-    document.write("Układ równań ma jedno rozwiązanie:<br>x = " + x + "<br>y = " + y);
+    if (W !== 0) {
+        var x = Wx / W;
+        var y = Wy / W;
+        document.write("Układ równań ma jedno rozwiązanie:<br>x = " + x + "<br>y = " + y);
     }
     else {
-        if (Wx != 0) {
+        if (Wx !== 0 || Wy !== 0) {
             document.write("<font color='red' size='5'>Układ równań jest sprzeczny (nie ma rozwiązań)!</font>");
         }
         else {
-            if (Wy != 0) {
-                document.write("<font color='red' size='5'>Układ równań ma nieskończoność rozwiązań</font>");
-            }
-            else {
-                document.write("<font color='red' size='5'>Układ równań nie ma rozwiązań!</font>");
-            }
+            document.write("<font color='red' size='5'>Układ równań ma nieskończenie wiele rozwiązań.</font>");
         }
     }
 }
